3a24b9a194ea05382060aef4fb6bb4ad8f0365a4
quang <flyhigh2608@gmail.com>
2026-07-31 11:29:56 +0700

    sua ten

--- project_custom/__manifest__.py
+++ project_custom/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "Connes Recruitment",
-    'summary': "Custom recruitment",
+    'name': "Project Custom",
+    'summary': "Project",
     'description': """
     """,
     'author': "Quang",
